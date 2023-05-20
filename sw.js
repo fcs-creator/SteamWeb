4179d2be32ddca8355ce9bda78c95dbffc17c4fd
fcs-creator <y-nishikawa@asobism.co.jp>
2023-05-20 09:46:50 +0900

    打ち間違えを修正

--- sw.js
+++ sw.js
@@ -1,31 +1,32 @@
+
 // ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
 
 // キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = "steam-web-cashes";
+var CACHE_NAME = 'steam-web-caches';
 var urlsToCache = [
-    '/pwa/',
-    '/pwa/css/style.css',
-    '/pwa/main.js'
-]
+	'/pwa/',
+	'/pwa/css/style.css',
+	'/pwa/main.js'
+];
 
-// インストール処理を登録
+// インストール処理
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
 });
 
-//リソースフェッチ時のキャッシュロード処理を登録
+// リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
 });
